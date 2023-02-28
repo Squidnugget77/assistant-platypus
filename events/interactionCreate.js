f527d7e2666225772112b695e0abb65c35248867
Luca G <luca.p.giannotti@gmail.com>
2023-02-27 19:10:50 -0600

    🌏 Ran embed messages and added a singular line :0

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,14 +1,11 @@
 const { EmbedBuilder, InteractionType } = require('discord.js');
 const fs = require('fs');
 
-const filename = 'maintenance.json';
-
-
 module.exports = (client, inter) => {
   if (inter.type === InteractionType.ApplicationCommand) {
     const DJ = client.config.opt.DJ;
     const command = client.commands.get(inter.commandName);
-    
+
     if (!command) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription('❌ | Error! Please contact Developers!')], ephemeral: true, }), client.slash.delete(inter.commandName)
     if (command.permissions && !inter.member.permissions.has(command.permissions)) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`❌ | You need do not have the proper permissions to exacute this command`)], ephemeral: true, })
     if (DJ.enabled && DJ.commands.includes(command) && !inter.member._roles.includes(inter.guild.roles.cache.find(x => x.name === DJ.roleName).id)) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`❌ | This command is reserved For members with \`${DJ.roleName}\` `)], ephemeral: true, })
@@ -28,4 +25,5 @@ module.exports = (client, inter) => {
       if (button) return button({ client, inter, customId, queue });
     }
   }
+//   if (inter.type === InteractionType.)
 };
